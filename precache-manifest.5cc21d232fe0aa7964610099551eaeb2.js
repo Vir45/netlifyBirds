ce6dfc233e2f9ff71874597bfe7f843602b35d0c
Vova <vir42@mail.ru>
2020-02-12 21:32:14 +0300

    d

---
+++ precache-manifest.5cc21d232fe0aa7964610099551eaeb2.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cafd63acbb37debf4791c3c3d37fd176",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8f2960bae6a880591549",
+    "url": "/static/css/2.7325e055.chunk.css"
+  },
+  {
+    "revision": "d86b74568eaa2a571639",
+    "url": "/static/css/main.fe652dbd.chunk.css"
+  },
+  {
+    "revision": "8f2960bae6a880591549",
+    "url": "/static/js/2.fd096a06.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.fd096a06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d86b74568eaa2a571639",
+    "url": "/static/js/main.7a2d4ae3.chunk.js"
+  },
+  {
+    "revision": "b4ae9a180a2ec9e2a559",
+    "url": "/static/js/runtime-main.804d465a.js"
+  },
+  {
+    "revision": "06a469380cb09a64d372f773665e7500",
+    "url": "/static/media/incognito.06a46938.jpg"
+  },
+  {
+    "revision": "4f82cd73f3446ef1af7fa6d3a302ef3a",
+    "url": "/static/media/logo.4f82cd73.svg"
+  },
+  {
+    "revision": "b2714b6abe42a251f97874c01b5dcb29",
+    "url": "/static/media/max.b2714b6a.gif"
+  }
+]);
